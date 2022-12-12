4d268140723b6db369e3c66c8e2633c19ad27a73
James Hughes <james.a.hughes@gmail.com>
2022-12-11 20:09:59 -0600

    shake nav logo on click

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#header__menu-toggle").click(function () {
     $(".nav-menu").fadeToggle("fast");
   });
+
+  $("#header__logo").click(function() {
+    $("#header__logo").effect("shake");
+  });
 });
