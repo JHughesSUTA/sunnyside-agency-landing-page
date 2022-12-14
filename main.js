0c15dc00a689098c42ef2e8f8e02303f761faad4
James Hughes <james.a.hughes@gmail.com>
2022-12-14 13:59:20 -0500

    fix section link placement on desktop

--- main.js
+++ main.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
   $(".link-placeholder").click(function () {
     $(this).effect("shake", {distance: 10});
   });
+
+  // TODO allow mobile menu to be closed by clicking outside of the menu element or on the hamburger menu
+  // $(document).mouseup((e) => {
+  //   var container = $(".nav-menu");
+  //   var hamburger = $("#header__menu-toggle");
+
+    // if ((!container.is(e.target) || !hamburger.is(e.target)) && container.has(e.target).length === 0) {
+    //   container.hide();
+    // }
+  // });
 });
