73753c01d49abd59954fdf3cd6b518976775c6cb
James Hughes <james.a.hughes@gmail.com>
2025-05-21 11:10:26 -0500

    ADA updates

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
-  $("#header__menu-toggle").click(function () {
-    $(".nav-menu").fadeToggle("fast");
+  const $toggleButton = $("#header__menu-toggle");
+  const $navMenu = $("#mobile-menu");
+
+  $toggleButton.on("click", function () {
+    const expanded = $toggleButton.attr("aria-expanded") === "true";
+
+    $toggleButton.attr("aria-expanded", (!expanded).toString());
+    $toggleButton.attr(
+      "aria-label",
+      expanded ? "Open main menu" : "Close main menu"
+    );
+
+    $navMenu.fadeToggle("fast");
   });
 
   $(".link-placeholder").click(function () {
-    $(this).effect("shake", {distance: 10});
+    $(this).effect("shake", { distance: 10 });
   });
-
-  // TODO allow mobile menu to be closed by clicking outside of the menu element or on the hamburger menu
-  // $(document).mouseup((e) => {
-  //   var container = $(".nav-menu");
-  //   var hamburger = $("#header__menu-toggle");
-
-    // if ((!container.is(e.target) || !hamburger.is(e.target)) && container.has(e.target).length === 0) {
-    //   container.hide();
-    // }
-  // });
 });
+
+// TODO allow mobile menu to be closed by clicking outside of the menu element or on the hamburger menu
+// $(document).mouseup((e) => {
+//   var container = $(".nav-menu");
+//   var hamburger = $("#header__menu-toggle");
+
+// if ((!container.is(e.target) || !hamburger.is(e.target)) && container.has(e.target).length === 0) {
+//   container.hide();
+// }
+// });
